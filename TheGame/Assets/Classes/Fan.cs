2ab7c08d4d111a3c53c292c2422a8b040153134e
AndraiShadow <drapeau.mp@gmail.com>
2013-01-27 09:34:20 -0500

    added fan animations

--- TheGame/Assets/Classes/Fan.cs
+++ TheGame/Assets/Classes/Fan.cs
@@ -101,7 +101,7 @@ public class Fan : MonoBehaviour {
 	}
 	
 	protected virtual void stopAnim(){
-		target.animation.Stop("Wind");
+		animation.Stop("Wind");
 	}
 	
 	//force objects
@@ -111,7 +111,6 @@ public class Fan : MonoBehaviour {
 	
 	//variables
 	protected GameObject target;
-	protected Animation animation;
 	protected float lastAttack;
 	protected float lastRegen;
 	protected float now;
